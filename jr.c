0d9db5fe89e3ae26df66666bd1b215d4309bb4e3
Danilo Horta <danilo.horta@pm.me>
2022-09-22 20:58:16 +0100

    fix, error

--- jr.c
+++ jr.c
@@ -97,11 +97,11 @@ int jr_parse(struct jr jr[], int length, char *json)
     struct jr_parser *p = get_parser(jr);
     struct jr_cursor *c = cursor(jr);
     int n = p->alloc_size - 2;
-    int rc = jr_parser_parse(p, c->length, c->json, n, nodes(jr));
-    if (rc) return rc;
+    error = jr_parser_parse(p, c->length, c->json, n, nodes(jr));
+    if (error) return error;
     sentinel_init(jr);
     if (p->size > 0) cnode(jr)->parent = -1;
-    return rc;
+    return error;
 }
 
 int jr_error(void) { return error; }
